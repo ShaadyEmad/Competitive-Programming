fbcd61548f312d9c8af6d14411ee335919ab71e2
Shady Emad <95425147+ShaadyEmad@users.noreply.github.com>
2024-08-11 07:03:15 +0300

    Update C. Showering.py

--- Codeforces/Codeforces Round 964 (Div. 4)/C. Showering.py
+++ Codeforces/Codeforces Round 964 (Div. 4)/C. Showering.py
@@ -1,1 +1,14 @@
+n = int(input())
+for _ in range(n):
+    n,s,m = map(int, input().split())
+    intervals = []
+    for i in range(n):
+        intervals.append(list(map(int, input().split())))     
+      
+    flag = False
+    for time in range(n):
+        if (time == 0 and intervals[time][0]-0>=s) or (intervals[time][0]-intervals[time-1][1]>=s) or (time == n-1 and m - intervals[time][1]>=s):
+            flag = True
+            break
 
+    print("YES" if flag else "NO")
