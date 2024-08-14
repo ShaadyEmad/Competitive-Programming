c7609f65fba5596af07b21cb030c6aeb7a66c746
Shady Emad <95425147+ShaadyEmad@users.noreply.github.com>
2024-08-14 23:57:46 +0300

    Delete Codeforces/Codeforces Round 966 (Div. 3)/C. Numeric String Template.py

--- Codeforces/Codeforces Round 966 (Div. 3)/C. Numeric String Template.py
+++
@@ -1,9 +0,0 @@
-for _ in range(int(input())):
-    lst_length = int(input())
-    lst = list(map(int, input().split()))
-    for __ in range(int(input())):
-        s = input() 
-        if (len(s) == lst_length) and (len(set(s))==len(set(lst))==len(set(zip(s,lst)))):
-            print("YES")
-        else:
-            print("NO")
