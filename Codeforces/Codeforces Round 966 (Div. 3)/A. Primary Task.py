986aabe0ce66daac6b5a12d9248c57299942bb96
Shady Emad <95425147+ShaadyEmad@users.noreply.github.com>
2024-08-14 04:32:51 +0300

    Delete Codeforces/Codeforces Round 966 (Div. 3) directory

--- Codeforces/Codeforces Round 966 (Div. 3)/A. Primary Task.py
+++
@@ -1,8 +0,0 @@
-for _ in range(int(input())):
-    num = input()
-    
-    if len(num) >= 3 and num.startswith('10') and int(num[2:])>=2 and not num[2:].startswith('0'):
-        print("YES")
-    
-    else:
-        print("NO")
