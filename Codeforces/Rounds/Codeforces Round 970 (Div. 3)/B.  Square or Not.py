e1c9944c6f8c3e9178081f2f1991aae3c811c76c
Shady Emad <95425147+ShaadyEmad@users.noreply.github.com>
2024-09-02 23:24:29 +0300

    Update B.  Square or Not.py

--- Codeforces/Rounds/Codeforces Round 970 (Div. 3)/B.  Square or Not.py
+++ Codeforces/Rounds/Codeforces Round 970 (Div. 3)/B.  Square or Not.py
@@ -1,15 +1,21 @@
 import math
 
-for _ in range(int(input()) ):
+def check_square(n):
+    root = int(math.sqrt(n))
+    return root * root == n, root
+
+t = int(input())
+for _ in range(t):
     l = int(input())
     s = input()
-    root = math.isqrt(l)
-    if root*root == l:
+    check, root = check_square(l)
+    if not check:
         print("NO")
     else:
-        zeros = s.count("0")
-        broder_elements = 4*(root-1)
-        if (zeros > l - broder_elements) or (zeros==0 and broder_elements!=l):
-            print("NO")
-        else:
+        borders = 4 * (root - 1)
+        zeros = l - borders
+        zeros_count = s.count("0")
+        if borders == l or zeros_count == zeros:
             print("YES")
+        else:
+            print("NO")
