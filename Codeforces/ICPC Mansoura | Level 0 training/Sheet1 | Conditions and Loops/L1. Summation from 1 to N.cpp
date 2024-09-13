37cf13f9094b8b6759ff9f52118dd2a3fdcd4c30
Shady Emad <95425147+ShaadyEmad@users.noreply.github.com>
2024-09-13 04:54:16 +0300

    Create L1. Summation from 1 to N.cpp

---
+++ Codeforces/ICPC Mansoura | Level 0 training/Sheet1 | Conditions and Loops/L1. Summation from 1 to N.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <list>
+#include <utility>
+using namespace std;
+
+int main(){
+    long long n; cin>>n;
+    cout << (long long) (n*(n+1))/2;
+    return 0;
+}
